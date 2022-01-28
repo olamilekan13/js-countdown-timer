74a7d2da359266be75954b429178ff7393504c74
olamilekan <olamilekanomosanya@gmail.com>
2022-01-28 13:23:59 +0100

    get time and add to the user  interface

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 const countDate = new Date("Jan 10 2022 13:05:55").getTime();
 let x = setInterval(function(){
     let now = new Date.getTime();
+
+    let distance = countDate - now;
+
+    let d = Math.floor(distance / (1000 *60*60*24));
+    let hrs = Math.floor((distance % (1000 * 60 * 60 * 24))/ (1000 * 60 *60));
+    let min = math.floor((distance % (1000 * 60 * 60)) / (1000*60));
+    let sec = math.floor(distance % (1000 * 60 * 60) / 1000);
+
+    //getting  the and attach to the i dof timer
+    
 })
